67b88cbaee60f9203689c377978af6a2539f2d62
maoxingpei <maoxingpei@hm5988.com>
2019-06-28 10:28:05 +0800

    package vendor

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,33 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=blog',
+            'username' => 'root',
+            'password' => '123456789',
+            'charset' => 'utf8',
+            'tablePrefix' => "db_"
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' =>false,//这句一定有，false发送邮件，true只是生成邮件在runtime文件夹下，不发邮件
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.qq.com',  //每种邮箱的host配置不一样
+                'username' => '934861215@qq.com',
+                'password' => 'qafrlfrtpvknbgag',
+                'port' => '25', //25
+                'encryption' => 'tls',
+            ],
+            'messageConfig'=>[
+                'charset'=>'UTF-8',
+                'from'=>['934861215@qq.com'=>'Clean Blog  * 旭尧']
+            ],
+        ],
     ],
 ];
